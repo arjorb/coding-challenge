842b69f8ffa4c36fc32fa5b0467b3663e5a042fa
John U <arjorb@gmail.com>
2023-05-23 15:42:15 +0200

    make the requested changes

--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -6,11 +6,19 @@ export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
  * @param {string} password
  * @returns {boolean}
  */
+
+const hasConsecutiveNumbers = (password) => {
+  const consecutiveNumbers = ["012", "123", "234", "345", "456", "567", "678", "789", "987", "876", "765", "654", "543", "432", "321", "210"];
+  return consecutiveNumbers.some((consecutive) => password.includes(consecutive));
+};
+
 export default function isValidPassword(password = "") {
   // The following line ensures, that password is always a string, like the number 128 -> string "128"
   if (typeof password !== "string") password = String(password);
 
   if (password.length !== 10) return false;
+
+  if (forbiddenPasswords.includes(password)) return false;
 
   // The password that only has numbers is invalid
   if (/^[0-9]*$/.test(password)) return false;
@@ -24,33 +32,10 @@ export default function isValidPassword(password = "") {
   // a password with only lower or upper case characters is invalid
   if (password === password.toLowerCase() || password === password.toUpperCase()) return false;
 
-  //  forbidden password are invalid
-  if (forbiddenPasswords.includes(password)) return false;
-
-  // a password with a directly ascending/descending sequence of numbers is invalid
-
-  const consecutiveNumbers = [
-    "012",
-    "123",
-    "234",
-    "345",
-    "456",
-    "567",
-    "678",
-    "789",
-    "987",
-    "876",
-    "765",
-    "654",
-    "543",
-    "432",
-    "321",
-    "210",
-  ].some((consecutive) => password.includes(consecutive));
-
-  if (consecutiveNumbers) return false;
+  if (hasConsecutiveNumbers(password)) return false;
 
   const setOfPassword = new Set([...password]);
   if (setOfPassword.size < 4) return false;
   return true;
 }
+
